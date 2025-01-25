bd50d34629e0fa05b28aa9aa4bbd01a4d6160a55
Bonfra <39456413+Bonfra04@users.noreply.github.com>
2025-01-25 15:44:11 +0100

    adding minimal widgets test

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
+import 'package:fvp_example/main.dart' as app;
+
 void main() async {
     IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
     test("3+3", () {
         expect(3 + 3, 6);
+    });
+
+    testWidgets("App", (WidgetTester tester) async {
+        app.main();
+        await tester.pumpAndSettle();
     });
 }
