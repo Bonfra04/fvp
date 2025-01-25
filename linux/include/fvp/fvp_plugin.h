d0cfce853a3e4f28817e79fc9f22b1b322515676
Bonfra <39456413+Bonfra04@users.noreply.github.com>
2025-01-25 11:57:39 +0100

    another test

--- linux/include/fvp/fvp_plugin.h
+++ linux/include/fvp/fvp_plugin.h
@@ -23,8 +23,8 @@ typedef struct {
 
 FLUTTER_PLUGIN_EXPORT GType fvp_plugin_get_type();
 
-FLUTTER_PLUGIN_EXPORT void fvp_plugin_register_with_registrar(
-    FlPluginRegistrar* registrar);
+// FLUTTER_PLUGIN_EXPORT void fvp_plugin_register_with_registrar(
+//     FlPluginRegistrar* registrar);
 
 G_END_DECLS
 
